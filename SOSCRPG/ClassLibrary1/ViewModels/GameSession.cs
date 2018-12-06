bfb9e19a2dcc6e8bc6cc02122a588bf50914cfff
Wojti <wdziuba@student.agh.edu.pl>
2018-12-06 13:15:02 +0100

    03.1 - 03.6

---
+++ SOSCRPG/ClassLibrary1/ViewModels/GameSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Wojti";
+            CurrentPlayer.Gold = 69;
+            CurrentPlayer.CharacterClass = "Nigger";
+            CurrentPlayer.ExperiencePoints = 2137;
+            CurrentPlayer.HitPoints = 100;
+            CurrentPlayer.Level = 1337;
+        }
+
+    }
+}
